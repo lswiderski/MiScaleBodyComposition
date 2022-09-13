0c4c5306c3ba0f909a425136eff2382788638dac
Lukasz Swiderski <lukasz@swiderski.xyz>
2022-09-13 16:52:27 +0200

    Added Date Test

--- tests/MiScaleBodyComposition.UnitTests/MiScaleBodyCompositionTest.cs
+++ tests/MiScaleBodyComposition.UnitTests/MiScaleBodyCompositionTest.cs
@@ -7,6 +7,7 @@ public class MiScaleBodyCompositionTest
 {
     private byte[] _validMiScaleData;
     private byte[] _stabilizedButWithoutImpendaceData;
+    private byte[] _dateData;
     private User _userInfo;
 
     [SetUp]
@@ -14,6 +15,7 @@ public class MiScaleBodyCompositionTest
     {
         _validMiScaleData = new byte[] { 27, 24, 2, 166, 230, 7, 2, 11, 17, 34, 7, 186, 1, 60, 55 };
         _stabilizedButWithoutImpendaceData = new byte[] { 27, 24, 2, 164, 230, 7, 8, 21, 16, 1, 25, 253, 255, 104, 56 };
+        _dateData = new byte[] { 27, 24, 2, 38, 230, 7, 9, 13, 14, 26, 38, 158, 1, 128, 57 };
         _userInfo = new User(182, 29, Sex.Male);
     }
 
@@ -33,6 +35,29 @@ public class MiScaleBodyCompositionTest
         var scale = new MiScale();
         var bc = scale.GetBodyComposition(_validMiScaleData, _userInfo);
         Assert.AreEqual(expectedResult, bc.Year);
+    }
+
+    [Test]
+    public void ShouldReturnProperDate()
+    {
+        int year = 2022;
+        int month = 9;
+        int day = 13;
+        int hour = 14;
+        int minutes = 26;
+        int seconds = 38;
+        var scale = new MiScale();
+        var bc = scale.GetBodyComposition(_dateData, _userInfo);
+        Assert.Multiple(() =>
+        {
+            Assert.AreEqual(year, bc.Year);
+            Assert.AreEqual(month, bc.Month);
+            Assert.AreEqual(day, bc.Day);
+            Assert.AreEqual(hour, bc.Hour);
+            Assert.AreEqual(minutes, bc.Minute);
+            Assert.AreEqual(seconds, bc.Second);
+        });
+
     }
 
     [Test]
